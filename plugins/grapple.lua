703f522883f68515cc70dc19878778ac55257d23
A-Lamia <git@lamia.io>
2022-12-23 15:21:08 +1100

    fix: grapple module name change

--- plugins/grapple.lua
+++ plugins/grapple.lua
@@ -1,7 +1,7 @@
 require("grapple").setup({
   setup = require("grapple.scope").fallback({
-    require("grapple.scope").resolvers.lsp_fallback,
-    require("grapple.scope").resolvers.git_fallback,
-    require("grapple.scope").resolvers.static
+    require("grapple").resolvers.lsp_fallback,
+    require("grapple").resolvers.git_fallback,
+    require("grapple").resolvers.static
   })
 })
