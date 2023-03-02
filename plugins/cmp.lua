637432b65c84ad6be1f16906cf5504a022eb93ea
A-Lamia <git@lamia.io>
2023-03-03 03:26:20 +1100

    perf(cmp): modify settings to lesson godot lag

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,8 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = {
+    performance = {
+      debounce = 300,
+      throttle = 120,
+      fetching_timeout = 100
+    },
     experimental = {
       ghost_text = true,
+    },
+    sources = {
+      { name = "nvim_lsp", priority = 1000 },
+      -- { name = "codeium",  priority = 750 },
+      { name = "luasnip",  priority = 700 },
+      { name = "path",     priority = 650 },
+      { name = "buffer",   priority = 400 },
     }
   }
 }
