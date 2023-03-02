3e1c693ebc576a66a4e862449b8edf5e289849d9
A-Lamia <git@lamia.io>
2023-03-03 03:06:47 +1100

    perf(plugins)!: disabled git in neo-tree for windows

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -3,14 +3,13 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     lazy = false,
     opts = {
-
       sources = {
         "filesystem",
       },
       add_blank_line_top = false,
       hide_root_node = true,
       retain_hidden_root_indent = true,
-      enable_git_status = true,
+      enable_git_status = false,
       source_selector = {
         winbar = false
       },
