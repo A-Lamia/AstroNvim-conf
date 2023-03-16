1115dfb569746b3a227ad06d975a14c12a027171
A-Lamia <git@lamia.io>
2023-03-17 00:11:56 +1100

    feat(plugins): add mason-null-ls settings

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,8 +8,28 @@ return {
         "neocmake",
         "jsonls",
         "pyright",
-        "lua_ls"
+        "lua_ls",
       },
     },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "black",
+        "prettierd",
+      },
+    },
+    config = function(_, opts)
+      local mason_null_ls = require "mason-null-ls"
+      local null_ls = require "null-ls"
+      mason_null_ls.setup(opts)
+      mason_null_ls.setup_handlers {
+        prettierd = function()
+          null_ls.register(null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "json", "markdown" } })
+        end,
+      }
+    end,
   },
 }
