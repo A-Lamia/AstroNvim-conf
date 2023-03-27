5333489976ef277ac83d9d2f92956293c5fd72f2
A-Lamia <git@lamia.io>
2023-03-27 18:29:52 +1100

    refactor(autocmd): remove dead code.

--- autocmd.lua
+++ autocmd.lua
@@ -1,18 +1,37 @@
--- vim.api.nvim_create_augroup("relative_number_switch", { clear = true })
--- vim.api.nvim_create_autocmd("InsertEnter", {
---   pattern = "*",
---   group = "relative_number_switch",
---   command = "set norelativenumber",
--- })
--- vim.api.nvim_create_autocmd("InsertLeave", {
---   pattern = "*",
---   group = "relative_number_switch",
---   command = "set relativenumber",
--- })
---
 vim.api.nvim_create_augroup("winblend", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "mason", "lazy", "grapple" },
+vim.api.nvim_create_autocmd("VimEnter", {
   group = "winblend",
-  command = "setlocal winblend=" .. vim.g.winblend,
+  callback = function()
+    if vim.g.neovide then
+      vim.o.winblend = vim.g.winblend
+      vim.cmd("hi! NormalFloat blend=" .. vim.g.winblend)
+    end
+  end,
+})
+
+-- vim.api.nvim_create_augroup("DiagnosticPopup", { clear = true })
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--   group = "DiagnosticPopup",
+--   callback = function()
+--     local row, col = vim.api.nvim_win_get_cursor(0)
+--     local line_diagnostic = vim.diagnostic.get(0, { lnum = row })
+--     if vim.lsp.buf.server_ready() then vim.diagnostic.open_float() end
+--   end,
+-- })
+
+vim.api.nvim_create_augroup("DiagnosticMode", { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = { "i", "v" },
+  group = "DiagnosticMode",
+  callback = function()
+    if vim.lsp.buf.server_ready() then vim.diagnostic.hide() end
+  end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "n",
+  group = "DiagnosticMode",
+  callback = function()
+    if vim.lsp.buf.server_ready() then vim.diagnostic.show() end
+  end,
 })
