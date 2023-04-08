7a370df3264f45bdd91271a623ab41acc4fa889d
A-Lamia <git@lamia.io>
2023-04-09 03:53:38 +1000

    fix(plugins): make custom neo-tree's config merge with astros

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,20 +2,22 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     lazy = false,
-    opts = {
-      sources = {
-        "filesystem",
-      },
-      add_blank_line_top = false,
-      hide_root_node = true,
-      retain_hidden_root_indent = true,
-      enable_git_status = false,
-      source_selector = {
-        winbar = false
-      },
-      git_status_async = false,
-      -- log_level = "trace",
-      -- log_to_file = true,
-    }
-  }
+    opts = function(_, opts)
+      return require("astronvim.utils").extend_tbl(opts, {
+        sources = {
+          "filesystem",
+        },
+        add_blank_line_top = false,
+        hide_root_node = true,
+        retain_hidden_root_indent = true,
+        enable_git_status = false,
+        source_selector = {
+          winbar = false,
+        },
+        git_status_async = false,
+        -- log_level = "trace",
+        -- log_to_file = true,
+      })
+    end,
+  },
 }
