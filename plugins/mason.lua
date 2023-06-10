516ed51bbd13b34a38e4cbe7739bcaf3c8bca425
A-Lamia <git@lamia.io>
2023-06-10 23:03:58 +1000

    fix(mason): function rename from setup_handlers to setup

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -25,7 +25,7 @@ return {
       local mason_null_ls = require "mason-null-ls"
       local null_ls = require "null-ls"
       mason_null_ls.setup(opts)
-      mason_null_ls.setup_handlers {
+      mason_null_ls.setup {
         prettierd = function()
           null_ls.register(null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "json", "markdown" } })
         end,
