28b25c54c1f19483ed5948baef70be94cb3abdff
A-Lamia <git@lamia.io>
2023-07-31 21:11:55 +1000

    refactor(plugins)!: remove nvim-surround

--- plugins/init.lua
+++ plugins/init.lua
@@ -181,24 +181,8 @@ return {
     lazy = false,
   },
 
-  {
-    "kylechui/nvim-surround",
-    event = "User AstroFile",
-    opts = {
-      keymaps = {
-        normal = "sa",
-        normal_cur = "sv",
-        normal_line = "ss",
-        normal_curl_line = "sS",
-        visual = "s",
-        delete = "sd",
-        change = "sr",
-      },
-      aliases = {
-        ["u"] = { "}", "]", ")", ">", '"', "'", "`" },
-      },
-    },
-  },
+  -- Text Manipulation --
+  -----------------------
 
   ------ git ------
   -----------------
