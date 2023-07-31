1b60c52bdf51c743e7792fb9850607b8a745ac0c
A-Lamia <git@lamia.io>
2023-07-31 20:40:30 +1000

    feat(neo-tree): update neo-tree settings

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,26 +1,25 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    lazy = false,
-    opts = function(_, opts)
-      return require("astronvim.utils").extend_tbl(opts, {
-        sources = {
-          "filesystem",
-        },
-        add_blank_line_top = false,
-        hide_root_node = true,
-        retain_hidden_root_indent = true,
-        enable_git_status = false,
-        source_selector = {
-          winbar = false,
-        },
-        git_status_async = false,
-        window = {
-          position = "right",
-        },
-        -- log_level = "trace",
-        -- log_to_file = true,
-      })
-    end,
-  },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "main",
+  -- lazy = false,
+  opts = function(_, opts)
+    return require("astronvim.utils").extend_tbl(opts, {
+      sources = {
+        "filesystem",
+      },
+      add_blank_line_top = true,
+      hide_root_node = true,
+      retain_hidden_root_indent = true,
+      enable_git_status = true,
+      source_selector = {
+        winbar = false,
+      },
+      git_status_async = true,
+      window = {
+        position = "right",
+      },
+      -- log_level = "trace",
+      -- log_to_file = true,
+    })
+  end,
 }
