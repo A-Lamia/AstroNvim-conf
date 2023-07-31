aaf06bd2aff890969852904e16aa6919908ff8c7
A-Lamia <git@lamia.io>
2023-07-31 21:02:48 +1000

    chore(init): formatting

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    _G.THEME = require("user.util.theme").setup()
-  end
+  callback = function() _G.THEME = require("user.util.theme").setup() end,
 })
 
 return {}
