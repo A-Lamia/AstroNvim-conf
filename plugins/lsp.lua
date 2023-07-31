44c83c36569858e117f9647abc4417d32511e1b6
A-Lamia <git@lamia.io>
2023-07-31 21:03:58 +1000

    feat(lsp): remove lsp-inlayhints and logic

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -9,52 +9,6 @@ return {
     },
   },
   {
-    "lvimuser/lsp-inlayhints.nvim",
-    branch = "anticonceal",
-    event = "LspAttach",
-    init = function()
-      vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = "LspAttach_inlayhints",
-        callback = function(args)
-          if not (args.data and args.data.client_id) then return end
-
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          local bufnr = args.buf
-
-          -- vim.api.nvim_buf_set_var(bufnr, "inlayhints_active", false)
-          if client.server_capabilities.inlayHintProvider then
-            local inlayhints = require "lsp-inlayhints"
-            inlayhints.on_attach(client, bufnr, false)
-            vim.api.nvim_buf_set_var(bufnr, "inlayhints_active", true)
-            -- if not enabled then inlayhints.toggle() end
-
-            local function toggle()
-              inlayhints.toggle()
-              if vim.g.inlayhints then
-                vim.g.inlayhints = false
-              else
-                vim.g.inlayhints = true
-              end
-            end
-            require("astronvim.utils").set_mappings({
-              n = {
-                ["<leader>uH"] = { toggle, desc = "Toggle inlay hints" },
-              },
-            }, { buffer = bufnr })
-          end
-        end,
-      })
-    end,
-    opts = {
-      enabled_at_startup = vim.g.inlayhints,
-      inlay_hints = {
-        highlight = "Comment",
-        only_current_line = false,
-      },
-    },
-  },
-  {
     "Maan2003/lsp_lines.nvim",
     event = "User AstroFile",
     config = function() require("lsp_lines").setup() end,
@@ -78,7 +32,7 @@ return {
     opts = function()
       return {
         server = require("astronvim.utils.lsp").config "rust_analyzer",
-        tools = { inlay_hints = { auto = false } },
+        -- tools = { inlay_hints = { auto = false } },
       }
     end,
   },
