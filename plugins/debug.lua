3ab6ecb9f544dabf75f00cac975bfbfc379d8f96
A-Lamia <git@lamia.io>
2023-08-04 23:24:26 +1000

    feat: add debug plugins for testing with astrotheme

---
+++ plugins/debug.lua
@@ -0,0 +1,25 @@
+return {
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- },
+  -- {
+  --   "rebelot/heirline.nvim",
+  --   optional = true,
+  --   opts = function(_, opts) opts.tabline = nil end,
+  -- },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   enabled = false,
+  --   lazy = false,
+  --   config = function()
+  --     local astrotheme = require "astrotheme.lib.util"
+  --     require("lualine").setup {
+  --       options = {
+  --         theme = function() return astrotheme.reload_module("lualine.themes.astrotheme", true) end,
+  --       },
+  --     }
+  --   end,
+  -- },
+}
