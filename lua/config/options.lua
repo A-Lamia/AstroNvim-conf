5e02597906240f95ad8c138695db55664c898569
A-Lamia <git@lamia.io>
2024-04-02 03:42:01 +1100

    feat(options): updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,27 +1,34 @@
 vim.g.neovide_padding_top = 10
 vim.g.neovide_scroll_animation_length = 0.25
+
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.g.neovide_cursor_vfx_particle_lifetime = 1.5
 vim.g.neovide_cursor_vfx_particle_density = 15
-vim.g.heirline_bufferline = true
-vim.g.inlay_hints_enabled = true
-vim.g.blend = (vim.g.neovide and 5) or (vim.env.WEZTERM and 3) or 0
+
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 3
+vim.g.neovide_light_angle_degrees = 15
+vim.g.neovide_light_radius = 25
+
+vim.g.blend = (vim.g.neovide and 0) or (vim.env.WEZTERM and 3) or 0
 vim.g.border = "none_cmp_fix"
 
-vim.o.guifont = "VictorMono_Nerd_Font,Symbols_Nerd_Font:h13.5:#e-subpixelantialias:#h-full"
+-- vim.opt.guifont = "VictorMono_Nerd_Font,Symbols_Nerd_Font:h13.5:#e-subpixelantialias:#h-full"
+-- vim.opt.guifont =
+--   "VictorMono_Nerd_Font_medium,Symbols_Nerd_Font:h13.5:m:#e-subpixelantialias:#h-full"
 
-vim.o.autoread = true
-vim.o.winwidth = 10
-vim.o.winminwidth = 10
-vim.o.equalalways = true
+vim.opt.foldcolumn = "0"
+vim.opt.autoread = true
+vim.opt.winwidth = 10
+vim.opt.winminwidth = 10
+vim.opt.equalalways = true
+vim.opt.winblend = vim.g.blend
+vim.opt.pumblend = vim.g.blend
 vim.o.winblend = vim.g.blend
 vim.o.pumblend = vim.g.blend
-vim.o.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
--- vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction vim.opt.SilentlyContinue tee;"
-vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
--- vim.opt.spell = true
+vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+vim.opt.shellxquote = ""
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+vim.opt.shellquote = ""
+vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
