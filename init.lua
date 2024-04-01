f84ee122e0273400ddd65230759a5676cbec97fc
A-Lamia <git@lamia.io>
2024-04-02 03:26:28 +1100

    feat(core): require options file
    AstroNvim does not auto-load options anymore.

--- init.lua
+++ init.lua
@@ -3,10 +3,9 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     _G.THEME = require("util.theme").setup()
   end,
 })
--- vim.api.nvim_command "<cmd>colors astrodark<cr>"
 
 require "config.lazy"
-
+require "config.options"
 require "config.keymaps"
 require "config.autocmd"
 require "config.polish"
