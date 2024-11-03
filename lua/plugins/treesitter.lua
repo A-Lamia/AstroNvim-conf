9ff03ebaf8721df5e599d4a74c3c740a56892649
A-Lamia <git@lamia.io>
2024-11-03 20:47:28 +1100

    feat(treesitter): add nushell; formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,67 +1,74 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  -- enabled = false,
-  init = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.asm = {
-      install_info = {
-        url = "https://github.com/rush-rs/tree-sitter-asm.git",
-        files = { "src/parser.c" },
-        branch = "main",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- enabled = false,
+    init = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.asm = {
+        install_info = {
+          url = "https://github.com/rush-rs/tree-sitter-asm.git",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+      }
+    end,
+    opts = {
+      compilers = { "gcc" },
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "rust",
+        "cpp",
+        "python",
       },
-    }
-  end,
-  opts = {
-    ensure_installed = {
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "rust",
-      "cpp",
-      "python",
-      "gdscript",
-      -- "gdresource"
+    },
+    dependencies = {
+      {
+        "nushell/tree-sitter-nu",
+        build = ":TSUpdate nu",
+      },
+      {
+        "rush-rs/tree-sitter-asm",
+        ft = "asm",
+      },
     },
   },
-  dependencies = {
-    {
-      "Wansmer/treesj",
-      keys = {
-        {
-          "sj",
-          desc = "Split Join Blocks Toggle",
-          function()
-            require("treesj").toggle()
-          end,
-        },
-      },
-      opts = {
-        use_default_keymaps = false,
+
+  {
+    "Wansmer/treesj",
+    keys = {
+      {
+        "sj",
+        desc = "Split Join Blocks Toggle",
+        function()
+          require("treesj").toggle()
+        end,
       },
     },
-    {
-      "Wansmer/sibling-swap.nvim",
-      keys = {
-        { "sl", desc = "Swap With Right" },
-        { "sh", desc = "Swap With Left" },
+    opts = {
+      compilers = { "clang" },
+      use_default_keymaps = false,
+    },
+  },
+  {
+    "Wansmer/sibling-swap.nvim",
+    keys = {
+      { "sl", desc = "Swap With Right" },
+      { "sh", desc = "Swap With Left" },
+    },
+    opts = {
+      keymaps = {
+        -- ["sl"] = "swap_with_right",
+        -- ["sh"] = "swap_with_left",
+        -- ['sL'] = 'swap_with_right_with_opp',
+        -- ['sH'] = 'swap_with_left_with_opp',
       },
-      opts = {
-        keymaps = {
-          -- ["sl"] = "swap_with_right",
-          -- ["sh"] = "swap_with_left",
-          -- ['sL'] = 'swap_with_right_with_opp',
-          -- ['sH'] = 'swap_with_left_with_opp',
-        },
-      },
     },
-    {
-      "nvim-treesitter/playground",
-      cmd = "TSPlaygroundToggle",
-    },
-    {
-      "rush-rs/tree-sitter-asm",
-      ft = "asm",
-    },
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
   },
 }
