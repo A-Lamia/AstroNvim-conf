c00a223752da3a6df68c317c6d619ab6e68a5919
A-Lamia <git@lamia.io>
2024-11-03 20:51:40 +1100

    feat(astrotheme): add check to disable color modifications
    same with heirline this change adds a check to see if modifications
    should be disabled, useful for when developing on astrotheme.

--- lua/plugins/astrotheme.lua
+++ lua/plugins/astrotheme.lua
@@ -1,7 +1,7 @@
 return {
   {
     "AstroNvim/astrotheme",
-    dev = true,
+    -- dev = true,
     lazy = false,
     priority = 1000,
     opts = {
@@ -31,30 +31,29 @@ return {
       highlights = {
         global = {
           modify_hl_groups = function(hl, c)
-            hl.Title.italic = true
-            hl.FloatTitle.italic = true
-            hl.TelescopePromptTitle.italic = true
-            hl.TelescopePreviewTitle.italic = true
-            --
-            -- local blend_fix = (vim.o.winblend == 0 and 0)
-            --   -- or (vim.g.neovide and 100)
-            --   or (vim.env.WEZTERM and vim.o.winblend)
-            --   or 0
-            hl.TelescopePromptNormal.blend = vim.o.winblend
-            hl.TelescopeResultsNormal.blend = vim.o.winblend
-            hl.TelescopePreviewNormal.blend = vim.o.winblend
+            local color = require "astrotheme.lib.color"
+            if not vim.g.default_heirline then
+              hl.Title.italic = true
+              hl.FloatTitle.italic = true
+              hl.TelescopePromptTitle.italic = true
+              hl.TelescopePreviewTitle.italic = true
 
-            hl.NormalFloat.blend = vim.o.winblend
-            hl.FloatBorder.blend = vim.o.winblend
-            hl.FloatTitle.blend = vim.o.winblend
+              hl.TelescopePromptNormal.blend = vim.o.winblend
+              hl.TelescopeResultsNormal.blend = vim.o.winblend
+              hl.TelescopePreviewNormal.blend = vim.o.winblend
 
-            -- hl.SpotlightNormalNC.bg = c.ui.tabline
+              hl.NormalFloat.blend = vim.o.winblend
+              hl.FloatBorder.blend = vim.o.winblend
+              hl.FloatTitle.blend = vim.o.winblend
 
-            hl.HeirlineYellow = { fg = c.ui.yellow }
-            hl.HeirlineGreen = { fg = c.ui.green }
-            hl.HeirlineTextInactive = { fg = c.ui.text_inactive }
+              -- hl.SpotlightNormalNC.bg = c.ui.tabline
 
-            hl.PmenuSel = { fg = c.ui.base, bg = c.ui.accent, bold = true, blend = 0 }
+              hl.HeirlineYellow = { fg = c.ui.yellow }
+              hl.HeirlineGreen = { fg = c.ui.green }
+              hl.HeirlineTextInactive = { fg = c.ui.text_inactive }
+
+              hl.PmenuSel = { fg = c.ui.base, bg = c.ui.accent, bold = true, blend = 0 }
+            end
             return hl
           end,
         },
